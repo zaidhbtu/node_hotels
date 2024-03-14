6a2c6b7612e71fe6b7726b08932c7649e34bdb16
Mohd Zaid <zaidhbtu@gmail.com>
2024-03-14 12:16:39 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose'); //mongoose improt
+require('dotenv').config();
 
-//Define the mongoDB connection URl
-const mongoURL = 'mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name
-
+//Define the mongoDB connection URL
+//const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL
 
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to my Hotel...How i can help you  ?')
@@ -19,7 +20,7 @@ const menuRoutes = require('./Routes/menuRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Listening on port 3000")
 })
 
